94ef8d176cbad505c6d9a45fd31fd5751b570683
rangaraj.s <rangaraj.sam@gmail.com>
2026-03-22 12:09:51 +0530

    removed domain variable

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,7 +2,6 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 const {ModuleFederationPlugin} = require('webpack').container;
 const packageJson = require('../package.json');
-const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
@@ -18,7 +17,7 @@ const prodConfig = {
                 './AuthorizationApp': './src/bootstrap',
             },
             remotes:{
-                container:`container@${domain}/container/latest/remoteEntry.js`
+                container:`container@/container/latest/remoteEntry.js`
             },
             shared: {
                 ...packageJson.dependencies,
